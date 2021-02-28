3ea040e6b9fd81a84996dfb791b09dc49db23c66
m4rch3n1ng <63159454+m4rch3n1ng@users.noreply.github.com>
2021-02-28 01:52:01 +0100

    fixed major issue with cookie.get(name) and made unencoded values accessable

--- src/cookie.js
+++ src/cookie.js
@@ -3,7 +3,7 @@ window.cookie = {
 		if (!name || typeof name != "string" || content == undefined) return false;
 
 		let path = typeof options?.path != "string" ? "/" : options.path;
-		let expires = +options?.expires && !isNaN(+options?.expires) ? new Date(Date.now() + 8.64e+7 * +options.expires).toUTCString() : "";
+		let expires = +options?.expires ? new Date(Date.now() + 8.64e+7 * +options.expires).toUTCString() : "";
 		content = typeof content == "object" ? JSON.stringify(content) : content;
 
 		document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(content)};path=${path};expires="${expires}`;
@@ -11,11 +11,16 @@ window.cookie = {
 		return true;
 	},
 	get: function (name, options) {
-		let cookies = document.cookie ? document.cookie.split("; ").map(cookie => cookie.split("=")).map(([key, value]) => [decodeURIComponent(key), decodeURIComponent(value)]) : [];
+		let cookies = document.cookie ? document.cookie.split("; ").map(cookie => cookie.split("=")).map(([k, ...v]) => [k, v.join("=")]) : [];
+		cookies = cookies.map(([key, val]) => {
+			try { key = decodeURIComponent(key) } catch { };
+			try { val = decodeURIComponent(val) } catch { };
+			return [key, val || ""];
+		});
 
 		if (!name || typeof name != "string") return name?.json ? Object.fromEntries(cookies) : cookies;
 
-		let value = cookies.find(([n]) => n == name)?.[0];
+		let value = cookies.find(([n]) => n == name)?.[1];
 
 		if (!options?.nojson && value) try { value = JSON.parse(value) } catch { };
 
